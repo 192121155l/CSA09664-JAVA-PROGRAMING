9f308a3c6034530f6acd95300a10559e11adfa10
192121155l <122265012+192121155l@users.noreply.github.com>
2023-01-12 14:48:34 +0530

    Create alphabetical.java

---
+++ alphabetical.java
@@ -0,0 +1,38 @@
+# CSA09664-JAVA-PROGRAMING
+import java.util.Scanner;
+class alphabetical
+{
+ public static void main(String[] args) 
+ {
+ int n;
+ String temp;
+ Scanner s1 = new Scanner(System.in);
+ System.out.print("Enter number of names you want to enter:");
+ n = s1.nextInt();
+ String names[] = new String[n];
+ 
+ System.out.println("Enter all the names:");
+ for(int i = 0; i < n; i++)
+ {
+ names[i] = s1.nextLine();
+ }
+ for (int i = 0; i < n; i++) 
+ {
+ for (int j = i + 1; j < n; j++) 
+ {
+ if (names[i].compareTo(names[j])>0) 
+ {
+ temp = names[i];
+ names[i] = names[j];
+ names[j] = temp;
+ }
+ }
+ }
+ System.out.print("Names in Sorted Order:");
+ for (int i = 0; i < n - 1; i++) 
+ {
+ System.out.print(names[i] + ",");
+ }
+ System.out.print(names[n - 1]);
+}
+}
