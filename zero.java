dd1021c5e03ce180f9b7098406825fa0ab903288
192121155l <122265012+192121155l@users.noreply.github.com>
2023-02-17 14:04:49 +0530

    Create zero.java

---
+++ zero.java
@@ -0,0 +1,16 @@
+# CSA09664-JAVA-PROGRAMING
+import java.util.*;
+class zero{
+public static void main (String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int count=o;
+int arr[]=(1,2,3,4,5,6,0,9,0,7);
+int n=arrlrnght;
+zero to end (arr,n);
+System.out.println("array after pashing zeros to the back");
+for(int i=0; i<n;i++)
+system.out.print(arr[i]+"");
+}
+} 
