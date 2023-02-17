2e081202ad74091aad83a55e6cd86935edf4462d
192121155l <122265012+192121155l@users.noreply.github.com>
2023-02-17 14:03:07 +0530

    Create uniquse.java

---
+++ uniquse.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class unique{
+public static void main(String []args)
+Scanner sc=new Scanner(System.in);
+String S=Sc.next();
+int count=0;
+for(i=0;i<s.lenght;i++){
+for(j=0;j<s.length;j++)
+{
+if(s.char At(i)==S.char At(j)
+{
+count=count+1;
+}
+if(count==0)
+{
+System.out.println(s.charAt(i));
+}
+else
+{
+count=0;
+}
+}
+}
